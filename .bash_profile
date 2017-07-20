fd80660e012bdddce1e67bac54d60e44f3713047
Marius Grigoriu <marius.grigoriu@me.com>
2017-07-20 13:37:14 -0700

    homebrew

--- .bash_profile
+++ .bash_profile
@@ -1,3 +1,6 @@
+# Install GNU core utilities (those that come with macOS are outdated).
+export PATH=`$(brew --prefix coreutils)/libexec/gnubin`:$PATH
+
 # Add `~/bin` to the `$PATH`
 export PATH="$HOME/bin:/usr/local/bin:$PATH";
 
--- brew.sh
+++ brew.sh
@@ -6,7 +6,7 @@
 brew update
 
 # Upgrade any already-installed formulae.
-brew upgrade --all
+brew upgrade
 
 # Install GNU core utilities (those that come with macOS are outdated).
 # Don’t forget to add `$(brew --prefix coreutils)/libexec/gnubin` to `$PATH`.
@@ -82,27 +82,30 @@ brew install xz
 
 # Install other useful binaries.
 brew install ack
+brew install awscli
 brew install dark-mode
 #brew install exiv2
 brew install git
 brew install git-lfs
 brew install imagemagick --with-webp
+brew install jq
 brew install kubernetes-cli
 brew install kubernetes-helm
-brew install lua
-brew install lynx
+#brew install lua
+#brew install lynx
+brew install npm
 brew install p7zip
-brew install pigz
-brew install pv
-brew install rename
-brew install rhino
-brew install speedtest_cli
-brew install ssh-copy-id
-brew install testssl
+#brew install pigz
+#brew install pv
+#brew install rename
+#brew install rhino
+#brew install speedtest_cli
+#brew install ssh-copy-id
+#brew install testssl
 brew install tree
-brew install vbindiff
-brew install webkit2png
-brew install zopfli
+#brew install vbindiff
+#brew install webkit2png
+#brew install zopfli
 
 
 # Remove outdated versions from the cellar.
