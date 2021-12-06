4ea56b12ace7dd78e58fe090e0314642d632efd3
Marius Grigoriu <marius.grigoriu@me.com>
2021-12-06 15:11:43 -0800

    minimize brew packages but add iterm and vscode

--- brew.sh
+++ brew.sh
@@ -10,49 +10,50 @@ brew upgrade
 
 # Install GNU core utilities (those that come with macOS are outdated).
 # Don’t forget to add `$(brew --prefix coreutils)/libexec/gnubin` to `$PATH`.
-brew install coreutils
-ln -s /usr/local/bin/gsha256sum /usr/local/bin/sha256sum
+# brew install coreutils
+# ln -s /usr/local/bin/gsha256sum /usr/local/bin/sha256sum
 
 # Install some other useful utilities like `sponge`.
 #brew install moreutils
 # Install GNU `find`, `locate`, `updatedb`, and `xargs`, `g`-prefixed.
-brew install findutils
+# brew install findutils
 # Install GNU `sed`, overwriting the built-in `sed`.
-brew install gnu-sed
+# brew install gnu-sed
 # Install Bash 4.
 # Note: don’t forget to add `/usr/local/bin/bash` to `/etc/shells` before
 # running `chsh`.
-brew install bash
-brew install bash-completion2
+# brew install bash
+# brew install bash-completion2
 
 # Switch to using brew-installed bash as default shell
-if ! fgrep -q '/usr/local/bin/bash' /etc/shells; then
-  echo '/usr/local/bin/bash' | sudo tee -a /etc/shells;
-  chsh -s /usr/local/bin/bash;
-fi;
+# if ! fgrep -q '/usr/local/bin/bash' /etc/shells; then
+  # echo '/usr/local/bin/bash' | sudo tee -a /etc/shells;
+  # chsh -s /usr/local/bin/bash;
+# fi;
 
 # Install `wget`
-brew install wget
+# brew install wget
 
 # Install more recent versions of some macOS tools.
-brew install vim
-brew install grep
-brew install openssh
-brew install screen
+# brew install vim
+# brew install grep
+# brew install openssh
+# brew install screen
 
 # Install font tools.
-brew tap bramstein/webfonttools
-brew install sfnt2woff
-brew install sfnt2woff-zopfli
-brew install woff2
+# brew tap bramstein/webfonttools
+# brew install sfnt2woff
+# brew install sfnt2woff-zopfli
+# brew install woff2
 
-brew install git
-brew install git-lfs
+# brew install git
+# brew install git-lfs
+brew install --cask iterm2
+brew install --cask visual-studio-code
 brew install go
-# brew install imagemagick --with-webp
 brew install jq
-brew install kubernetes-cli
-brew install kubernetes-helm
-brew install tree
+# brew install kubernetes-cli
+# brew install kubernetes-helm
+# brew install tree
 # Remove outdated versions from the cellar.
 brew cleanup
